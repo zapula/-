906b4514cebf6109cccdb20609df777cab1366c2
zapula <130806048+zapula@users.noreply.github.com>
2023-11-27 22:30:19 +1000

    1.4
    структура дашборда

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ df = pd.read_csv('C:\Data\website_data.csv')  # Укажите здесь пут
 app = dash.Dash(__name__)
 
 # Определение структуры дашборда
+app.layout = html.Div([
+    html.H1('Анализ данных о посещении веб-сайта', style={'textAlign': 'center'}),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': date, 'value': date} for date in df['Дата'].unique()],
+        value=df['Дата'].iloc[0],
+        clearable=False,
+        style={'width': '50%', 'margin': '0 auto'}
+    ),
+    dcc.Graph(id='line-chart'),
+    dcc.Graph(id='histogram'),
+    dcc.Graph(id='pie-chart'),
+    dcc.Graph(id='box-plot'),
+    dcc.Graph(id='scatter-plot')
+], style={'padding': '20px'})
+
+# Определение логики дашборда
